84d263c4e3846a1f00d526a0bd130188dbfe3938
ey2167 <fzreo338@gmail.com>
2014-10-16 17:16:17 -0700

    assignmetns

---
+++ AsteriskCube.Java
@@ -0,0 +1,28 @@
+/*Elohe Yonas CSC200 project problem 1. the asterisk line
+ * using system.out.println, i can convey the messages of 
+ * what i type into java. using this method, i need to 
+ * create a string of code to convey the asterisk cube
+ * when I run the program
+ */
+
+import java.util.Scanner;
+public class AsteriskCube
+{
+	public static void main(String[]args)
+	//this could be messy if i mess up the formation...
+		{
+		System.out.println ("    *******************");
+		System.out.println ("   *                 **");
+		System.out.println ("  *                 * *");
+		System.out.println (" *                 *  *");
+		System.out.println ("*******************   *");
+		System.out.println ("*                 *   *");
+		System.out.println ("*                 *   *");
+		System.out.println ("*                 *   *");
+		System.out.println ("*                 *   *");
+		System.out.println ("*                 *  * ");
+		System.out.println ("*                 * *  ");
+		System.out.println ("*******************    ");
+		//well that's the first step done...now compiling..
+	}
+}
